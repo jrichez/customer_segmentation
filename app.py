cf1f317263330ac03fa89058b1ee3e3c952ec368
jrichez <richez06@hotmail.fr>
2022-01-24 23:38:18 +0100

    Version 4

--- app.py
+++ app.py
@@ -1,48 +1,56 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 24 14:45:46 2022
+
+@author: riche
+"""
+
 import streamlit as st
 import pandas as pd
 import numpy as np
 import pickle
+from pandas.io.parsers import ParserError
 
 # Head
 
 st.title('Segmentation client')
-st.markdown("Cette application retourne les segments attribues aux clients de deux manieres : ")
+st.markdown("Cette application retourne les segments attribués aux clients de deux manières : ")
 st.markdown(" - Une prediction pour un client unique")
-st.markdown(" - Un fichier client complete par une colonne contenant les segments")
+st.markdown(" - Un fichier client complété par une colonne contenant les segments")
 
 # Left
 
 title_pred = st.sidebar.title("Client unique")
-st.sidebar.subheader("Selectionner les differents attributs du nouveau client et le modele retournera le segment auquel il appartient")
+st.sidebar.subheader("Sélectionner les différents attributs du nouveau client et le modèle retournera le segment auquel il appartient")
 
 with st.sidebar.form("my_form"):
 	gender = st.selectbox('Sexe', ('Homme', 'Femme'))
 	gender_map = {'Homme' : 'Male', 'Femme' : 'Female'}
 	gender = pd.Series(gender).map(gender_map)[0]
 
-	ever_married = st.selectbox('A deja ete marrie', ('Oui', 'Non'))
+	ever_married = st.selectbox('A déjà été marrié', ('Oui', 'Non'))
 	married_map = {'Oui' : 'Yes', 'Non' : 'No'}
 	ever_married = pd.Series(ever_married).map(married_map)[0]
 
 	age = round(st.number_input('Age', step=1))
 
-	graduated = st.selectbox('Diplome', ('Oui', 'Non'))
+	graduated = st.selectbox('Diplôme', ('Oui', 'Non'))
 	graduated_map = {'Oui' : 'Yes', 'Non' : 'No'}
 	graduated = pd.Series(graduated).map(graduated_map)[0]
 
-	profession = st.selectbox('Profession', ('Sante', 'Ingenieur', 'Avocat', 'Divertissement', 'Artiste', 'Executant', 'Docteur', 'Artisan', 'Marketing'))
-	profession_map = {'Sante' : 'Healthcare', 'Ingenieur' : 'Engineer', 'Avocat' : 'Lawyer', 'Divertissement' : 'Entertainment', 'Artiste' : 'Artist', 'Executant' : 'Executive', 'Docteur' : 'Doctor', 'Artisan' : 'Homemaker', 'Marketing' : 'Marketing'}
+	profession = st.selectbox('Profession', ('Santé', 'Ingénieur', 'Avocat', 'Divertissement', 'Artiste', 'Executant', 'Docteur', 'Artisan', 'Marketing'))
+	profession_map = {'Santé' : 'Healthcare', 'Ingénieur' : 'Engineer', 'Avocat' : 'Lawyer', 'Divertissement' : 'Entertainment', 'Artiste' : 'Artist', 'Executant' : 'Executive', 'Docteur' : 'Doctor', 'Artisan' : 'Homemaker', 'Marketing' : 'Marketing'}
 	profession = pd.Series(profession).map(profession_map)[0]
 
-	work_experience = round(st.number_input("Nombre d'annees d'experience professionelle", step=1))
+	work_experience = round(st.number_input("Nombre d'années d'expérience professionelle", step=1))
 
-	spending_score = st.selectbox('Depense moyenne', ('Basse', 'Moyenne', 'Elevee'))
-	spending_map = {'Basse' : 'Low', 'Moyenne' : 'Average', 'Elevee' : 'High'}
+	spending_score = st.selectbox('Dépense moyenne', ('Basse', 'Moyenne', 'Elevée'))
+	spending_map = {'Basse' : 'Low', 'Moyenne' : 'Average', 'Elevée' : 'High'}
 	spending_score = pd.Series(spending_score).map(spending_map)[0]
 
 	family_size = round(st.number_input('Taille de la famille', step=1))
 
-	var_1 = st.selectbox('Categorie', ('1', '2', '3', '4', '5', '6', '7'))
+	var_1 = st.selectbox('Catégorie', ('1', '2', '3', '4', '5', '6', '7'))
 	var_1_map = {'1' : 'Cat_1', '2' : 'Cat_2', '3' : 'Cat_3', '4' : 'Cat_4', '5' : 'Cat_5', '6' : 'Cat_6', '7' : 'Cat_7'}
 	var_1 = pd.Series(var_1).map(var_1_map)[0]
 
@@ -70,7 +78,7 @@ with st.sidebar.form("my_form"):
 	model = pickle.load(open('customer_segmentation.sav', 'rb'))
 	pred = model.predict(new_customer)
 
-	submitted = st.form_submit_button("Predire")
+	submitted = st.form_submit_button("Prédire")
 
 	if submitted:
 		st.subheader('Ce client appartient au segment ' + str(pred))
@@ -82,7 +90,7 @@ st.markdown('Le fichier doit avoir la structure suivante :')
 
 new_customer_1 = np.array(['Male', 'Yes', 60, 'Yes', 'Artist', 1, 'High', 5, 'Cat_4'])
 new_customer_2 = np.array(['Female', 'No', 26, 'Yes', 'Healthcare', 3, 'Low', 1, 'Cat_6'])
-df_col = ['Sexe', 'A deja ete marrie', 'Age', 'Diplome', 'Profession', "Nombre d'annees d'experience", 'Depense moyenne', 'Taille de la famille', 'Categorie']
+df_col = ['Sexe', 'A déjà été marrié', 'Age', 'Diplôme', 'Profession', "Nombre d'années d'expérience", 'Dépense moyenne', 'Taille de la famille', 'Catégorie']
 df = pd.DataFrame([new_customer_1, new_customer_2], columns=df_col)
 df_view = st.table(df)
 
@@ -98,29 +106,33 @@ def convert_df(df):
    return df.to_csv(index=False).encode('utf-8')
 
 if customer_file is not None:
-    
-   customer_file = pd.read_csv(customer_file)
-   df_to_predict = customer_file.copy()
-   df_to_predict.iloc[:, 0] = encoder_gender.transform(df_to_predict.iloc[:, 0])
-   df_to_predict.iloc[:, 1] = encoder_ever_married.transform(df_to_predict.iloc[:, 1])
-   df_to_predict.iloc[:, 3] = encoder_graduated.transform(df_to_predict.iloc[:, 3])
-   df_to_predict.iloc[:, 4] = encoder_profession.transform(df_to_predict.iloc[:, 4])
-   df_to_predict.iloc[:, 6] = encoder_spending_score.transform(df_to_predict.iloc[:, 6])
-   df_to_predict.iloc[:, 8] = encoder_var_1.transform(df_to_predict.iloc[:, 8])
+   try:
+       customer_file = pd.read_csv(customer_file)
+       df_to_predict = customer_file.copy()
+       
+       df_to_predict.iloc[:, 0] = encoder_gender.transform(df_to_predict.iloc[:, 0])
+       df_to_predict.iloc[:, 1] = encoder_ever_married.transform(df_to_predict.iloc[:, 1])
+       df_to_predict.iloc[:, 3] = encoder_graduated.transform(df_to_predict.iloc[:, 3])
+       df_to_predict.iloc[:, 4] = encoder_profession.transform(df_to_predict.iloc[:, 4])
+       df_to_predict.iloc[:, 6] = encoder_spending_score.transform(df_to_predict.iloc[:, 6])
+       df_to_predict.iloc[:, 8] = encoder_var_1.transform(df_to_predict.iloc[:, 8])
 
-   pred = model.predict(scaler.transform(df_to_predict))
-   customer_file['Segment'] = pred
-   df_csv = convert_df(customer_file)
-
-   st.download_button('Fichier client complete', df_csv, file_name='fichier_client.csv')
+       pred = model.predict(scaler.transform(df_to_predict))
+       customer_file['Segment'] = pred
+       df_csv = convert_df(customer_file)
+       
+       st.success('Les segments ont pu être prédits et le fichier complété est téléchargeable ci-dessous')
+       st.download_button('Fichier client complété', df_csv, file_name='fichier_client.csv')
+       
+   except ParserError:
+       st.error('Erreur : Le fichier doit être au format csv')
+   except TypeError:
+       st.error("Erreur : Le fichier n'a pas la bonne structure")
+           
    
 test = pd.read_csv('test.csv')
 test = convert_df(test)
 
-"""
-traduire en anglais 
-inclure try except pour generer exception si le fichier na pas la bonne taille
-"""
 
 with st.expander('Note'):
 	st.write("Un fichier est disponible pour pouvoir tester l'application : ")
